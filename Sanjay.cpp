b2adb5a5b55e8580db472b30ab31c206b20b77da
SanjayPrajapati22 <sanjayprajapati842394@gmail.com>
2026-05-24 19:50:21 +0530

    Add files via upload

---
+++ Sanjay.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Header file for input/output
+
+int main() { // Execution starts here
+    std::cout << "Hello World!"; // Prints text to the console
+    return 0; // Signals that the program finished successfully
+}
